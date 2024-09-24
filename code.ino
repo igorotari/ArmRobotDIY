e1e32c598e6903790b40ebf881621594fbea7472
Igor <igrt204@gmail.com>
2024-09-24 12:45:59 +0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,97 +1,95 @@
-#include <Servo.h> // подключаем библиотеку для работы с сервоприводом
-Servo myservo1; // создаем объект для управления сервоприводом 1
-Servo myservo2; // создаем объект для управления сервоприводом 2
-Servo myservo3; // создаем объект для управления сервоприводом 3
-Servo myservo4; // создаем объект для управления сервоприводом 4
+#include <Servo.h> // include the library for working with the servo motor
+Servo myservo1; // create objects to control servos
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
 
-int pos1 = 80, pos2 = 60, pos3 = 130, pos4 = 0; // задаем начальные углы для сервоприводов
+int pos1 = 80, pos2 = 60, pos3 = 130, pos4 = 0; // set initial angles for the servos
 
 void setup() {
-// подключаем сервомоторы к соответствующим пинам на плате
-myservo1.attach(A1); // сервомотор 1 подключаем к контакту A1
-myservo2.attach(A0); // сервомотор 2 подключаем к контакту A0
-myservo3.attach(6); // сервомотор 3 подключаем к контакту D6
-myservo4.attach(9); // сервомотор 4 подключаем к контакту D9
+// attach the servos to the corresponding pins on the board
+myservo1.attach(A1);
+myservo2.attach(A0);
+myservo3.attach(6);
+myservo4.attach(9);
 
-// устанавливаем начальные углы для сервоприводов
-myservo1.write(pos1); // сервомотор 1 поворачивается на угол pos1
-delay(1000); // ждем 1 секунду
-myservo2.write(pos2); // сервомотор 2 поворачивается на угол pos2
-myservo3.write(pos3); // сервомотор 3 поворачивается на угол pos3
-myservo4.write(pos4); // сервомотор 4 поворачивается на угол pos4
-delay(1500); // ждем 1.5 секунды
+// set the initial positions for the servos
+myservo1.write(pos1);
+delay(1000);
+myservo2.write(pos2);
+myservo3.write(pos3);
+myservo4.write(pos4);
+delay(1500);
 }
 
 void loop() {
-// поворот вправо
-for (pos1; pos1 > 0; pos1–) {
-myservo1.write(pos1); // уменьшение угла сервомотора 1 (вращение вправо)
-delay(5); // задержка 5 мс для регулировки скорости
+// turn to the right
+for (pos1; pos1 > 0; pos1--) {
+myservo1.write(pos1);
+delay(5);
 }
-delay(1000); // задержка 1 секунда
+delay(1000);
 
-// открытие клешни
+// open the claw
 for (pos4; pos4 < 100; pos4++) {
-myservo4.write(pos4); // увеличение угла сервопривода 4 (клешня открывается)
+myservo4.write(pos4);
 }
-delay(1000); // задержка 1 секунда
+delay(1000); //
 
-// сервомотор 2 поворачивается до угла 50 градусов
-for (pos2; pos2 > 50; pos2–) {
-myservo2.write(pos2); // уменьшение угла сервомотора 2
-delay(5); // задержка 5 мс для регулировки скорости
+// rotate servo 2 to 50 degrees
+for (pos2; pos2 > 50; pos2--) {
+myservo2.write(pos2);
+delay(5);
 }
 
-// сервомотор 3 поворачивается до угла 50 градусов
-for (pos3; pos3 > 50; pos3–) {
-myservo3.write(pos3); // уменьшение угла сервомотора 3
-delay(5); // задержка 5 мс для регулировки скорости
+// rotate servo 3 to 50 degrees
+for (pos3; pos3 > 50; pos3--) {
+myservo3.write(pos3);
+delay(5);
 }
-delay(1500); // задержка 1.5 секунды
+delay(1500);
 
-// закрытие клешни
-for (pos4; pos4 > 0; pos4–) {
-myservo4.write(pos4); // уменьшение угла сервомотора 4 (клешня закрывается)
+// close the claw
+for (pos4; pos4 > 0; pos4--) {
+myservo4.write(pos4);
 }
-delay(1000); // задержка 1 секунда
+delay(1000);
 
-// сервомотор 3 поворачивается до угла 120 градусов, подъем рычага
+// rotate servo 3 to 120 degrees, lifting the arm
 for (pos3; pos3 < 120; pos3++) {
-myservo3.write(pos3); // увеличение угла сервомотора 3
-delay(5); // задержка 5 мс для регулировки скорости
+myservo3.write(pos3);
+delay(5);
 }
-delay(1000); // задержка 1 секунда
+delay(1000);
 
-// поворот влево
+// turn to the left
 for (pos1; pos1 < 180; pos1++) {
-myservo1.write(pos1); // увеличение угла сервомотора 1 (вращение влево)
-delay(5); // задержка 5 мс для регулировки скорости
+myservo1.write(pos1);
+delay(5);
 }
-delay(1000); // задержка 1 секунда
+delay(1000);
 
-// опускание руки
-for (pos3; pos3 > 50; pos3–) {
-myservo3.write(pos3); // уменьшение угла сервомотора 3
-delay(5); // задержка 5 мс для регулировки скорости
+// lower the arm
+for (pos3; pos3 > 50; pos3--) {
+myservo3.write(pos3);
+delay(5);
 }
-delay(1000); // задержка 1 секунда
-
-// открытие клешни
+delay(1000);
+// open the claw
 for (pos4; pos4 < 100; pos4++) {
-myservo4.write(pos4); // увеличение угла сервомотора 4 (клешня открывается)
+myservo4.write(pos4);
 }
-delay(1000); // задержка 1 секунда
+delay(1000);
 
-// подъем руки
+// lift the arm
 for (pos3; pos3 < 120; pos3++) {
-myservo3.write(pos3); // увеличение угла сервомотора 3
-delay(5); // задержка 5 мс для регулировки скорости
+myservo3.write(pos3);
+delay(5);
 }
-delay(1000); // задержка 1 секунда
+delay(1000);
 
-// закрытие клешни
-for (pos4; pos4 > 0; pos4–) {
-myservo4.write(pos4); // уменьшение угла сервомотора 4 (клешня закрывается)
+// close the claw
+for (pos4; pos4 > 0; pos4--) {
+myservo4.write(pos4);
 }
-delay(1000); // задержка 1 секунда
-}
+delay(1000);
